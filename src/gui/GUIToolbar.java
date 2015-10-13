5859ec8f255d27e57ca69dad761e1e70b5750768
John Dai <johnrundai@gmail.com>
2015-10-13 18:50:03 -0400

    added a webview for displaying 'help '

--- src/gui/GUIToolbar.java
+++ src/gui/GUIToolbar.java
@@ -6,13 +6,16 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Separator;
 import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 
@@ -21,50 +24,24 @@ public class GUIToolbar extends GUIComponent {
     private SLogoLanguage myLanguage;
     private List<GUITurtle> myTurtles;
     private ToolBar toolBar;
+    private GUITurtleAreaRedrawInterface myTurtleArea;
+    private Stage myStage;
 
     public GUIToolbar (Stage stage, List<GUITurtle> turtles, GUITurtleAreaRedrawInterface turtleArea) {
-        Button openImage = new Button("Open Image");
         myTurtles=turtles;
-        openImage.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle (ActionEvent event) {
-                FileChooser fileChooser = new FileChooser();
-                fileChooser.setTitle("Select Image for Turtle");;
-                fileChooser.getExtensionFilters().addAll(
-                                                         new ExtensionFilter("Image Files", "*.png", "*.jpg", ".bmp", ".gif"));
-                File selectedFile = fileChooser.showOpenDialog(stage);
-                if (selectedFile!=null) {
-                    try {
-                        Image image = new Image("file:"+selectedFile.getAbsolutePath());
-                        for (GUITurtle t: myTurtles) {
-                            t.setImage(image);
-                        }
-                        turtleArea.drawAll();
-                    }
-                    catch (Exception e) {
-                        System.out.println("file:"+selectedFile.getAbsolutePath());
-                        System.out.println(e);
-                    }
-                }
-            }
-            
-        });
+        myTurtleArea=turtleArea;
+        myStage=stage;
+        
         toolBar = new ToolBar(
-                              new Button("New"),
-                              new Button("Open"),
-                              new Button("Save"),
+                              new Button("Language"),
                               new Separator(),
-                              openImage,
-                              new Button("Run"),
+                              addImageFileLoader(),
                               new Separator(),
-                              new Button("Debug"),
-                              new Button("Profile"));
+                              addHelpButton());
     }
 
     @Override
     public Node returnNodeToDraw () {
-        // TODO Auto-generated method stub
         return toolBar;
     }
 
@@ -76,16 +53,60 @@ public class GUIToolbar extends GUIComponent {
 
     }
 
-    private void addHelpButton () {
-
+    private Button addHelpButton () {
+        Button help = new Button("Help");
+        help.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle (ActionEvent event) {
+                Popup popup = new Popup();
+                popup.setX(myStage.getX());
+                popup.setY(myStage.getY());
+                Button hide = new Button("Hide");
+                hide.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        popup.hide();
+                    }
+                });
+                WebView html = new WebView();
+                html.getEngine().load("http://www.cs.duke.edu/courses/compsci308/fall15/assign/03_slogo/commands.php");
+                popup.getContent().addAll(html);
+                popup.getContent().addAll(hide);
+                popup.show(myStage);
+            }
+        });
+        return help;
     }
 
     private void addLanguageDropDown () {
 
     }
 
-    private void addImageFileLoader () {
-
+    private Button addImageFileLoader () {
+        Button openImage = new Button("Open Image");
+        openImage.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle (ActionEvent event) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Select Image for Turtle");;
+                fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", ".bmp", ".gif"));
+                File selectedFile = fileChooser.showOpenDialog(myStage);
+                if (selectedFile!=null) {
+                    try {
+                        Image image = new Image("file:"+selectedFile.getAbsolutePath());
+                        for (GUITurtle t: myTurtles) {
+                            t.setImage(image);
+                        }
+                        myTurtleArea.drawAll();
+                    }
+                    catch (Exception e) {
+                        //TODO: error handling
+                        System.out.println("file:"+selectedFile.getAbsolutePath());
+                        System.out.println(e);
+                    }
+                }
+            }
+        });
+        return openImage;
     }
-
 }
