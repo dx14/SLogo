e5c186a4615659bc635d75e5ab790117dc91dfe8
John Dai <johnrundai@gmail.com>
2015-10-26 22:28:15 -0400

    refactored statedisplays

--- src/gui/GUIImageDisplay.java
+++ src/gui/GUIImageDisplay.java
@@ -1,38 +1,28 @@
 package gui;
 
-import java.util.Observable;
-import java.util.Observer;
 import javafx.collections.ListChangeListener;
-import javafx.scene.Node;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
-import parser.structure.TurtleContainer;
 
-public class GUIImageDisplay extends GUIComponent implements ListChangeListener{
-    GUITurtleAreaImagesInterface myTurtleArea;
-    ScrollPane pane;
-    HBox hbox;
-    public GUIImageDisplay(GUITurtleAreaImagesInterface turtleArea) {
-        myTurtleArea=turtleArea;
-        hbox = new HBox();
-        pane = new ScrollPane(hbox);
+
+public class GUIImageDisplay extends GUIStateDisplay implements ListChangeListener {
+    private GUITurtleAreaImagesInterface myTurtleArea;
+
+    public GUIImageDisplay (GUITurtleAreaImagesInterface turtleArea) {
+        myTurtleArea = turtleArea;
         myTurtleArea.addImagesListener(this);
     }
-    public void redraw() {
-        hbox.getChildren().clear();
+
+    @Override
+    public void redraw () {
+        getHbox().getChildren().clear();
         int max = myTurtleArea.getImagesSize();
-        for (int i=0; i<max;i++) {
-            hbox.getChildren().add(new Text(i + "->"));
-            hbox.getChildren().add(new ImageView(myTurtleArea.parseImage(i)));
+        for (int i = 0; i < max; i++) {
+            getHbox().getChildren().add(new Text(i + "->"));
+            getHbox().getChildren().add(new ImageView(myTurtleArea.parseImage(i)));
         }
     }
-    @Override
-    public Node returnNodeToDraw () {
-        redraw();
-        return pane;
-    }
+
     @Override
     public void onChanged (Change arg0) {
         redraw();
--- src/gui/GUIPaletteDisplay.java
+++ src/gui/GUIPaletteDisplay.java
@@ -1,40 +1,31 @@
 package gui;
 
-import javafx.collections.ListChangeListener;
-import javafx.collections.ListChangeListener.Change;
 import javafx.collections.MapChangeListener;
-import javafx.scene.Node;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
-public class GUIPaletteDisplay extends GUIComponent implements MapChangeListener{
-    GUITurtleAreaPaletteInterface myTurtleArea;
-    ScrollPane pane;
-    HBox hbox;
-    public GUIPaletteDisplay(GUITurtleAreaPaletteInterface turtleArea) {
-        myTurtleArea=turtleArea;
-        hbox = new HBox();
-        pane = new ScrollPane(hbox);
+
+public class GUIPaletteDisplay extends GUIStateDisplay implements MapChangeListener {
+    private GUITurtleAreaPaletteInterface myTurtleArea;
+
+    public GUIPaletteDisplay (GUITurtleAreaPaletteInterface turtleArea) {
+        super();
+        myTurtleArea = turtleArea;
         myTurtleArea.addPaletteListener(this);
     }
-    public void redraw() {
-        hbox.getChildren().clear();
-        for (Integer i: myTurtleArea.getColorMap().keySet()) {
-            hbox.getChildren().add(new Text(i + "->"));
-            Rectangle rect = new Rectangle(20,20);
+
+    @Override
+    public void redraw () {
+        getHbox().getChildren().clear();
+        for (Integer i : myTurtleArea.getColorMap().keySet()) {
+            getHbox().getChildren().add(new Text(i + "->"));
+            Rectangle rect = new Rectangle(20, 20);
             rect.setFill(Paint.valueOf(myTurtleArea.getColorMap().get(i)));
-            hbox.getChildren().add(rect);
+            getHbox().getChildren().add(rect);
         }
     }
-    @Override
-    public Node returnNodeToDraw () {
-        redraw();
-        return pane;
-    }
+
     @Override
     public void onChanged (Change arg0) {
         redraw();
